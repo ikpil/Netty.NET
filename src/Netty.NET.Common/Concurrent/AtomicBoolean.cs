58b7c5d9aebcd8486fa16c094fd13082a8f31559
ikpil <ikpil0@gmail.com>
2025-07-10 06:52:19 +0900

    java style atomic int, boolean

---
+++ src/Netty.NET.Common/Concurrent/AtomicBoolean.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+
+namespace Netty.NET.Common.Concurrent;
+
+public class AtomicBoolean
+{
+    private int _location;
+
+    public AtomicBoolean() : this(false)
+    {
+    }
+
+    public AtomicBoolean(bool initialValue)
+    {
+        _location = initialValue ? 1 : 0;
+    }
+
+    public bool read()
+    {
+        return Volatile.Read(ref _location) != 0;
+    }
+
+    // true if successful, otherwise false if the witness value was not the same as the expectedValue.
+    public bool compareAndSet(bool expectedValue, bool newValue)
+    {
+        var expectedInt = expectedValue ? 1 : 0;
+        var newInt = newValue ? 1 : 0;
+        var original = Interlocked.CompareExchange(ref _location, newInt, expectedInt);
+        return original == expectedInt;
+    }
+}
---
+++ src/Netty.NET.Common/Concurrent/AtomicInteger.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading;
+
+namespace Netty.NET.Common.Concurrent;
+
+public class AtomicInteger
+{
+    private int _location;
+
+    public AtomicInteger() : this(0)
+    {
+    }
+
+    public AtomicInteger(int location)
+    {
+        _location = location;
+    }
+
+    public int incrementAndGet()
+    {
+        return Interlocked.Increment(ref _location);
+    }
+
+    public int getAndIncrement()
+    {
+        var next = Interlocked.Increment(ref _location);
+        return next - 1;
+    }
+
+    public int decrementAndGet()
+    {
+        return Interlocked.Decrement(ref _location);
+    }
+
+    public int read()
+    {
+        return Volatile.Read(ref _location);
+    }
+
+    public int exchange(int exchange)
+    {
+        return Interlocked.Exchange(ref _location, exchange);
+    }
+
+    public int decrease(int value)
+    {
+        return Interlocked.Add(ref _location, -value);
+    }
+
+    public bool compareAndSet(int expectedValue, int newValue)
+    {
+        var original = Interlocked.CompareExchange(ref _location, newValue, expectedValue);
+        return original == expectedValue;
+    }
+
+    public int add(int value)
+    {
+        return Interlocked.Add(ref _location, value);
+    }
+}
