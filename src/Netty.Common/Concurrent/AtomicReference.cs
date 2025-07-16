4598d1039b7e372cf7cfc77ba520a574dbb2a6c9
ikpil <ikpil0@gmail.com>
2025-07-17 08:55:38 +0900

    added AtomicReference

---
+++ src/Netty.Common/Concurrent/AtomicReference.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+
+namespace Netty.NET.Common.Concurrent;
+
+public class AtomicReference<T> where T : class
+{
+    private T _location;
+
+    public AtomicReference(T location = null)
+    {
+        _location = location;
+    }
+
+    public T get()
+    {
+        return Volatile.Read(ref _location);
+    }
+
+    public void set(T newValue)
+    {
+        Volatile.Write(ref _location, newValue);
+    }
+
+    public T getAndSet(T value)
+    {
+        return Interlocked.Exchange(ref _location, value);
+    }
+
+    public bool compareAndSet(T expectedValue, T newValue)
+    {
+        var original = Interlocked.CompareExchange(ref _location, newValue, expectedValue);
+        return original == expectedValue;
+    }
+
+    public override string ToString()
+    {
+        var value = get();
+        return value?.ToString() ?? "null";
+    }
+}
